32d2198165e2652f2886faf5aa065acf62cad3fb
Michael Fladischer <FladischerMichael@fladi.at>
2019-02-28 10:15:56 +0100

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*names, **kwargs):
 
 setup(
     name='outpost.attendance.browser',
-    version='0.1.1',
+    version='0.1.2',
     license='BSD',
     description='Outpost Attendance Browser',
     long_description='%s\n%s' % (
