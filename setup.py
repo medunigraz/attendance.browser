0b8ed4da7f90b5b465a2de5fed96ab8074f7f7ec
Michael Fladischer <FladischerMichael@fladi.at>
2019-05-24 12:36:08 +0200

    Use correct python3 classifier as we can only deploy and test on Python 3.5 due to Raspbian stretch.

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
         'Operating System :: POSIX',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Utilities',
     ],
